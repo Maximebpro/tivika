40fb36ac14c730c8aa9fe9059c7e6d8b5c8bb691
Maxime Bernard <maxime.bernard.pro@gmail.com>
2017-03-28 13:40:38 +0200

    chore(api): init nodejs api

---
+++ api/index.ts
@@ -0,0 +1,21 @@
+// require('ts-node/register');
+
+import * as http from "http";
+import { } from "http";
+import * as url from "url";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import errorHandler = require("errorhandler");
+import methodOverride = require("method-override");
+
+let api = express();
+
+api.set('port', (process.env.PORT || 5000));
+
+api.get('/', function (request, response) {
+  response.json('Hello');
+});
+
+api.listen(api.get('port'), function () {
+  console.log('Node app is running on port', api.get('port'));
+});
