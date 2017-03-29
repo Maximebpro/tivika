d3d9fd772b81bd77e087371fb9d89a186862bff6
Maxime Bernard <maxime.bernard.pro@gmail.com>
2017-03-29 13:34:55 +0200

    add script to deploy on heroku

---
+++ deploy-api.sh
@@ -0,0 +1,3 @@
+## ⬢ enigmatic-journey-88005
+## https://enigmatic-journey-88005.herokuapp.com/ | https://git.heroku.com/enigmatic-journey-88005.git
+git subtree push --prefix api https://git.heroku.com/enigmatic-journey-88005.git master
