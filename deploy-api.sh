34ab8ad97479d0047cf3e1c2f40f64460c8af37b
Maxime Bernard <maxime.bernard.pro@gmail.com>
2017-03-29 13:37:03 +0200

    add header to deploy script

--- deploy-api.sh
+++ deploy-api.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ## ⬢ enigmatic-journey-88005
 ## https://enigmatic-journey-88005.herokuapp.com/ | https://git.heroku.com/enigmatic-journey-88005.git
 git subtree push --prefix api https://git.heroku.com/enigmatic-journey-88005.git master
